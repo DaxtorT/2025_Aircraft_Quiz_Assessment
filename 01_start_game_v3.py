f46cd8f0cc90d5e4acb09efe8e5f25f32c686666
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2025-04-10 11:44:28 +1200

    V3.1 of Start Game (Decided to use .place to place the widgets)

---
+++ 01_start_game_v3.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+class StartGame():
+    """
+    Aircraft Quiz Start Game Screen
+    """
+
+    def __init__(self):
+        """
+        Start Game GUI
+        """
+
+        bg_path = "C:/Users/scrap/OneDrive - Massey High School/Year 13 (2025)/TCCOUE/Aircraft Quiz/Documentation/aircraft_background.jpg"  # Use correct relative path
+        bg_img = Image.open(bg_path)
+        resize_img = bg_img.resize((500, 720), Image.Resampling.LANCZOS)  # Resize to fit window
+        self.bg_img = ImageTk.PhotoImage(resize_img)
+
+
+        self.start_bg = Label(root, image=self.bg_img)
+        self.start_bg.place(x=0, y=0, relwidth=1, relheight=1)
+        self.start_bg.image = self.bg_img
+
+        # self.start_frame = Frame(root)
+        # self.start_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+        self.start_heading = Label(root,
+                                   text="Welcome To\nThe Best Aircraft Quiz",
+                                   font=("B612", "16", "bold"), width=20, border=1, relief="solid")
+        self.start_heading.place(x=120, y=50)
+        
+        self.start_instruct = Label(root,
+                                   text="*Placeholder Instructions*",
+                                   font=("B612", "12", "bold"), width=40, border=1, relief="solid")
+        self.start_instruct.place(x=50, y=150)
+
+
+
+# Main Routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Colour Quest")
+    root.geometry("500x720")
+    root.resizable(False, False)
+    #root.grid_rowconfigure(0, weight=1)
+    #root.grid_columnconfigure(0, weight=1)
+    StartGame()
+    root.mainloop()
